b9570421e6f71c91de5928b07cabdb23acf568a2
Brian Mosbeux <b.mosbeux@gmail.com>
2024-09-09 13:31:40 +0200

    Take into account today's date when saving stocs_dfs csv files

--- get_stock_data.py
+++ get_stock_data.py
@@ -1,6 +1,7 @@
 import bs4 as bs
 import datetime as dt
 import os
+import pandas as pd
 import pickle
 import requests
 import yfinance as yf
@@ -18,33 +19,35 @@ def save_sp500_tickers():
 		pickle.dump(tickers, f)
 	return tickers
 
-def download_stocks_as_csv_files(tickers, start=dt.datetime(2000, 1, 1), end=dt.datetime.today()):
+def download_stocks_as_csv_files(tickers, output_folder, start=dt.datetime(2000, 1, 1), end=dt.datetime.today()):
 	for ticker in tickers:
 		print(ticker)
-		if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+		if not os.path.exists('{}/{}.csv'.format(output_folder, ticker)):
 			df = yf.download(ticker, start, end)
-			df.to_csv('stock_dfs/{}.csv'.format(ticker))
+			df.to_csv('{}/{}.csv'.format(output_folder, ticker))
 		else:
 			print('Already have {}'.format(ticker))
 
 def get_sp500_data_from_yahoo(reload_sp500=False):
+	start = dt.datetime(2000, 1, 1)
+	end = dt.datetime.now()
+	date_string = end.strftime('%Y%m%d')
+	output_folder = '{}_stock_dfs'.format(date_string)
 	if reload_sp500:
 		tickers = save_sp500_tickers()
 	else:
 		with open('sp500tickers.pickle', 'rb') as f:
 			tickers = pickle.load(f)
-	if not os.path.exists('stock_dfs'):
-		os.makedirs('stock_dfs')
-	start = dt.datetime(2000, 1, 1)
-	end = dt.datetime.now()
-	download_stocks_as_csv_files(tickers=tickers, start=start, end=end)
+	if not os.path.exists(output_folder):
+		os.makedirs(output_folder)
+	download_stocks_as_csv_files(tickers=tickers, start=start, end=end, output_folder=output_folder)
 
-def compile_adjusted_close_data():
+def compile_adjusted_close_data(input_folder):
 	with open('sp500tickers.pickle', 'rb') as f:
 		tickers = pickle.load(f)
 	main_df = pd.DataFrame()
 	for count, ticker in enumerate(tickers):
-		df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+		df = pd.read_csv('{}/{}.csv'.format(input_folder, ticker))
 		df.set_index('Date', inplace=True)
 		df.rename(columns={'Adj Close': ticker}, inplace=True)
 		df.drop(labels=['Open', 'High', 'Low', 'Close', 'Volume'], axis=1, inplace=True)
