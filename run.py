3b566a4c1bff741b80436ca95ba4b1241be01244
Brian Mosbeux <b.mosbeux@gmail.com>
2024-09-10 16:33:52 +0200

    create buy_sell_hold function that creates label for marchine learning algorithm
    The idea is to map this function to a pandas dataframe column and that column will be the label
    pass in any number of columns (*args). in practice these columns will be our future price change columns that were processed in process_data_for_labels.
    
    If the price went up 2% it is a buy, if it went down 2% it is a sell, else it is a hold

--- run.py
+++ run.py
@@ -11,3 +11,13 @@ def process_data_for_labels(ticker):
 		df['{}_{}d'.format(ticker, i)] = (df[ticker].shift(-i) - df[ticker]) / df[ticker]
 	df.fillna(0, inplace=True)
 	return tickers, df
+
+def buy_sell_hold(*args):
+	cols = [c for c in args]
+	requirement = 0.02
+	for col in cols:
+		if col > requirement:
+			return 1
+		if col < -requirement:
+			return -1
+	return 0
