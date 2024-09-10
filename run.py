104fd6688928d9be8a0d5380b2dc668152feba2c
Brian Mosbeux <b.mosbeux@gmail.com>
2024-09-10 19:10:32 +0200

    create extract_featuresets function
    First process the data for label (process_data_for_labels)
    Second label the data by mapping the buy_sell_hold function to the dataframe and create a new column with these newly genereate buy(1), sell(-1), hold(0) labels
    clean the dataset for infinite values and nan
    Create new columns for each ticker percentage price change from one day to the other
    Return these new columns values as X and the labels as y

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 
+from collections import Counter
+import numpy as np
 import pandas as pd
 
 def process_data_for_labels(ticker):
@@ -10,7 +12,7 @@ def process_data_for_labels(ticker):
 	for i in range(1, how_many_days+1):
 		df['{}_{}d'.format(ticker, i)] = (df[ticker].shift(-i) - df[ticker]) / df[ticker]
 	df.fillna(0, inplace=True)
-	return tickers, df
+	return how_many_days, tickers, df
 
 def buy_sell_hold(*args):
 	cols = [c for c in args]
@@ -21,3 +23,19 @@ def buy_sell_hold(*args):
 		if col < -requirement:
 			return -1
 	return 0
+
+def extract_featuresets(ticker):
+	how_many_days, tickers, df = process_data_for_labels(ticker)
+	df['{}_target'.format(ticker)] = list(map(buy_sell_hold, *[df['{}_{}d'.format(ticker,i)] for i in range(1,how_many_days+1)]))
+	vals = df['{}_target'.format(ticker)].values
+	str_vals = [str(i) for i in vals]
+	print('Data spread', Counter(str_vals))
+	df.fillna(0, inplace=True)
+	df = df.replace([np.inf, -np.inf], np.nan)
+	df.dropna(inplace=True)
+	df_vals = df[[ticker for ticker in tickers]].pct_change()
+	df_vals = df_vals.replace([np.inf, -np.inf], 0)
+	df_vals.fillna(0, inplace=True)
+	X = df_vals.values 
+	y = vals
+	return X, y, df
